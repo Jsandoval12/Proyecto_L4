0cab9c1e223d8e25d38fccee6342211cbb5a295f
Jonathan Sandoval <jonathansandovaal12@gmail.com>
2022-02-07 19:56:49 -0600

    Creando estructura basica

---
+++ Honduras_Shopping/Honduras_Shopping.BL/ProductosBL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Honduras_Shopping.BL
+{
+    public class ProductosBL
+    {
+       public  List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+
+            producto1.Id = 1;
+            producto1.Descripcion = "Productos Varios";
+            producto1.Precio = 200;
+
+            var producto2 = new Producto();
+
+            producto2.Id = 2;
+            producto2.Descripcion = "Productos Varios";
+            producto2.Precio = 500;
+
+            var producto3 = new Producto();
+
+            producto3.Id = 3;
+            producto3.Descripcion = "Productos Varios";
+            producto3.Precio = 300;
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return listadeProductos;
+
+        }
+    }
+}
---
+++ Honduras_Shopping/Honduras_Shopping.Win/Form1.cs
@@ -0,0 +1,32 @@
+﻿using Honduras_Shopping.BL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Honduras_Shopping.Win
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
+
+            foreach (var Productos  in listadeProductos)
+            {
+                MessageBox.Show(Productos.Descripcion);
+            } 
+        }
+    }
+}
